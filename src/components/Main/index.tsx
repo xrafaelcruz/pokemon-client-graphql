fbaaec8fe743db336de6796a5444d29f683dc414
Rafael Cruz <xrafaelcruz@gmail.com>
2020-08-15 16:31:50 -0300

    removed svgs

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,5 @@
 import React from 'react'
 
-import logo from 'assets/images/logo.svg'
-import heroIllustration from 'assets/images/hero-illustration.svg'
-
 import * as S from './styles'
 
 const Main = ({
@@ -10,16 +7,8 @@ const Main = ({
   description = 'TypeScript, ReactJS, NextJS e Styled Components'
 }) => (
   <S.Wrapper>
-    <S.Logo
-      src={logo}
-      alt="Imagem de um átomo e React Avançado escrito ao lado"
-    />
     <S.Title>{title}</S.Title>
     <S.Description>{description}</S.Description>
-    <S.Illustration
-      src={heroIllustration}
-      alt="Um desenvolvedor de frente para uma tela com código."
-    />
   </S.Wrapper>
 )
 
